aaab2bd4a4cc1663c4057263701e454f29c1839c
Fatma Ashraf <129579797+FattemaAshraf@users.noreply.github.com>
2023-12-03 17:39:10 +0200

    added some enhancement

--- Food_Recipe/src/app/guards/auth.guard.ts
+++ Food_Recipe/src/app/guards/auth.guard.ts
@@ -1,11 +1,14 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../auth/services/auth.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
+  const authService= inject(AuthService);
+  const router = inject(Router);
   if(localStorage.getItem('userToken') !== null){
     return true;
   } else{
-    inject(Router).createUrlTree(['/auth']);
+    router.navigate(['/auth'])
     return false;
   }
 };
