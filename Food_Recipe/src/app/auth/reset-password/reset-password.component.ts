564227aba1a63e0ca2b12b241d43d36f7ec29725
Fatma Ashraf <129579797+FattemaAshraf@users.noreply.github.com>
2023-12-06 23:08:12 +0200

    added custom validator on confirm pass

--- Food_Recipe/src/app/auth/reset-password/reset-password.component.ts
+++ Food_Recipe/src/app/auth/reset-password/reset-password.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
-import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { AuthService } from '../services/auth.service';
 import { ToastrService } from 'ngx-toastr';
 import { MatDialog } from '@angular/material/dialog';
@@ -7,7 +12,7 @@ import { MatDialog } from '@angular/material/dialog';
 @Component({
   selector: 'app-reset-password',
   templateUrl: './reset-password.component.html',
-  styleUrls: ['./reset-password.component.scss']
+  styleUrls: ['./reset-password.component.scss'],
 })
 export class ResetPasswordComponent {
   hide = true;
@@ -46,11 +51,17 @@ export class ResetPasswordComponent {
     public dialog: MatDialog
   ) {}
 
-  passwordMatchValidator(control: AbstractControl) {
-    return control.value.password === control.value.confirmPassword
-      ? null
-      : { mismatch: true };
+  passwordMatchValidator(control: any) {
+    if (control.get('password') === control.get('confirmPassword')) {
+      return null;
+    } else {
+      control
+        .get('confirmPassword')
+        ?.setErrors({ invalid: 'password and confirm password not match' });
+      return { invalid: 'password and confirm password not match' };
+    }
   }
+
   onSubmit(data: FormGroup) {
     console.log(data.value);
     this._authService.onResetPassword(data.value).subscribe({
